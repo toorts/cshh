cc0eb4ce26859c34bf1fad12d7226d1a4e79762e
Martin House <torchester@gmail.com>
2022-10-23 12:52:35 +0500

    add intro method

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 99;
+int b1 = 43;
+int c1 = 53;
+
+int a2 = 44;
+int b2 = 87;
+int c2 = 42;
+
+int a3 = 47;
+int b3 = 97;
+int c3 = 73;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
