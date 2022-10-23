a216ab2793d4ee0c9770e685b2cc3601862dec60
Martin House <torchester@gmail.com>
2022-10-23 15:55:12 +0500

    add method array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 12, 54, 42, 55, 67, 23, 17, 34, 23 };
+
+int n = array.Length;
+
+int find = 23;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; // index = index + 1
+}
