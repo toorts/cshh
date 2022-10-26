da321e1aedd79aa600f0de3863befd71348e97b8
Martin House <torchester@gmail.com>
2022-10-26 17:47:41 +0500

    add methods

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,68 @@
+﻿// //* Вид 1
+
+// void Method1()
+// {
+//     Console.WriteLine("Hello There");
+// }
+
+// Method1();
+
+// //* Вид 2
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+
+// string message = "Hello Hello";
+// Method2(message);
+// // Или
+// Method2("Hello World");
+// ///
+// Console.WriteLine();
+// ///
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+
+// Method21("Hello Harry", 4);
+// // Или
+// Method21(msg: "Harry", count: 4);
+// // Или
+// Method21(count: 4, msg: "Hello");
+
+// //* Вид 3
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+
+// Console.WriteLine(year);
+
+//* Вид 4
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+
+    return result;
+}
+
+string res = Method4(4, "ewrq");
+Console.WriteLine(res);
